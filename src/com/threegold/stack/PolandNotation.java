ae6aac81a7d09496308df5f07a2f045ae74de911
ZhouXin <zhouxin1630@163.com>
2022-05-17 11:13:56 +0800

    中缀表达式字符串转中缀表达式列表

--- src/com/threegold/stack/PolandNotation.java
+++ src/com/threegold/stack/PolandNotation.java
@@ -11,13 +11,35 @@ public class PolandNotation {
         /*String suffixExpression = "3 4 + 5 * 6 -";
         List<String> rpnList = getListString(suffixExpression);
         int res = calculate(rpnList);
-        System.out.printf("%s = %d\n", suffixExpression, res);*/
-    }
+        System.out.printf("后缀表达式%s = %d\n", suffixExpression, res);*/
 
+        // 完成中缀表达式转后缀表达式的功能
+        String expression = "1+((2+3)*4)-5";
+        List<String> infixList = toInfixExpressionList(expression);
+        System.out.println(infixList);
+        // System.out.printf("中缀表达式 %s = %d");
+    }
 
     // 将中缀表达式转成对应的 List
     public static List<String> toInfixExpressionList(String expression) {
         List<String> res = new ArrayList<>();
+        int i = 0;  // 这是一个指针，用于遍历中缀表达式字符串
+        String str; // 对于多位数的拼接
+        char c; // 每遍历到一个字符，就放入 c
+        do {
+            if ((c = expression.charAt(i)) < 48 || (c = expression.charAt(i)) > 57) {  // 如果 c 是一个非数字，直接放入到 res
+                res.add(String.valueOf(c));
+                i++;
+            } else {  // 如果是一个数，则需要考虑多位数
+                str = "";
+                while (i < expression.length() && 
+                    ((c = expression.charAt(i)) >= 48 && (c = expression.charAt(i)) <= 57)) {
+                        str += c;
+                        i++;
+                }
+                res.add(str);
+            }
+        } while (i < expression.length());
         return res;
     }
 
