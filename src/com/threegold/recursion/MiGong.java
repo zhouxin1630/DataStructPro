a8c9210f3f00c2d85d44a7be150e0b8771646f79
ZhouXin <zhouxin1630@163.com>
2022-05-17 19:13:59 +0800

    修改了移动的方向进行测试

--- src/com/threegold/recursion/MiGong.java
+++ src/com/threegold/recursion/MiGong.java
@@ -20,7 +20,6 @@ public class MiGong {
         // 设置挡板
         map[3][1] = 1;
         map[3][2] = 1;
-        map[2][3] = 1;
 
         // 输出地图
         for (int i = 0; i < 8; i++) {
@@ -31,7 +30,7 @@ public class MiGong {
         }
 
         // 使用递归回溯给小球找路
-        setWay(map, 1, 1);
+        setWay2(map, 1, 1);
         // 输出新的地图, 小球走过并标识过的地图
         System.out.println("输出新的地图, 小球走过并标识过的地图:");
         for (int i = 0; i < 8; i++) {
@@ -65,6 +64,31 @@ public class MiGong {
                 } else if (setWay(map, i - 1, j)) { // 向上走
                     return true;
                 } else if (setWay(map, i, j - 1)) { // 向左走
+                    return true;
+                } else { // 说明该点走不通
+                    map[i][j] = 3;
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // 修改找路策略，改成 上 -> 右 -> 下 -> 左
+    public static boolean setWay2(int[][] map, int i, int j) {
+        if (map[6][5] == 2) { // 通路已经找到
+            return true;
+        } else {
+            if (map[i][j] == 0) { // 当前这个点没有走过
+                map[i][j] = 2; // 假定该点可以走通
+                if (setWay2(map, i - 1, j)) { // 向上走
+                    return true;
+                } else if (setWay2(map, i, j + 1)) { // 向右走
+                    return true;
+                } else if (setWay2(map, i + 1, j)) { // 向下走
+                    return true;
+                } else if (setWay2(map, i, j - 1)) { // 向左走
                     return true;
                 } else { // 说明该点走不通
                     map[i][j] = 3;
